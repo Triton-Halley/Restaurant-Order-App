112e46bbffdf986de304f1ebcb136149eec60605
Triton-Halley <Triton.halley@outlook.com>
2022-05-20 22:04:53 +0430

    header component finished for now

--- order-app/src/Components/Header/Header.js
+++ order-app/src/Components/Header/Header.js
@@ -8,9 +8,20 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>ReactMeals</h1>
+        <h1>Triton Meal</h1>
         <HeaderCartButton />
       </header>
+      <div className={classes.description}>
+        <h2>Delicious Food, Delivered To You</h2>
+        <p>
+          Choose Your Favorite meal from our broad selection of available meals
+          and enjoy a delicious lunch or dinner at home.
+        </p>
+        <p>
+          All our meals are cooked with high-quality ingredients, just-in-time
+          and og course by experienced chefs!
+        </p>
+      </div>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
