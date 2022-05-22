bb7183e0653bb4427700a05a458621338e520288
Triton-Halley <Triton.halley@outlook.com>
2022-05-22 18:45:16 +0430

    add effect to cart button with useEffect

--- order-app/src/Components/Header/HeaderCartButton.js
+++ order-app/src/Components/Header/HeaderCartButton.js
@@ -1,18 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import AddToCartContext from "../../Context/AddToCart";
 import modal from "../../Context/ShowModal";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighed] = useState(false);
   const ctx = useContext(AddToCartContext);
   const ctxModal = useContext(modal);
   const showModal = (e) => {
     ctxModal.showModal();
   };
+
+  const { Orders } = ctx;
+  const ButtonClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (Orders.length === 0) {
+      return;
+    }
+    setBtnIsHighlighed(true);
+    setTimeout(() => {
+      setBtnIsHighlighed(false);
+    }, 300);
+  }, [Orders]);
+
   return (
     <>
-      <button onClick={showModal} className={classes.button}>
+      <button onClick={showModal} className={ButtonClasses}>
         <span className={classes.icon}>
           <CartIcon />
         </span>
